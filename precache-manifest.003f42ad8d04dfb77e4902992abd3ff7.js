58ffea61ab7b7a2753c382c625cf13fe9b848521
Jordan Cotter <jrdn.cttr@gmail.com>
2020-08-06 21:38:32 -0500

    Updates

---
+++ precache-manifest.003f42ad8d04dfb77e4902992abd3ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8bdf6f9ef47cd0b4d1",
+    "url": "/cotterscs/static/css/main.1a4fa007.chunk.css"
+  },
+  {
+    "revision": "2b8bdf6f9ef47cd0b4d1",
+    "url": "/cotterscs/static/js/main.2b8bdf6f.chunk.js"
+  },
+  {
+    "revision": "22d88fc37c1257cf8cad",
+    "url": "/cotterscs/static/js/runtime~main.22d88fc3.js"
+  },
+  {
+    "revision": "4861ac1479509577020a",
+    "url": "/cotterscs/static/js/2.4861ac14.chunk.js"
+  },
+  {
+    "revision": "f3425aed80431c6fb2a56e279b6240c0",
+    "url": "/cotterscs/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cotterscs/precache-manifest.08edda1bfb2c3cb7307d60ab9e2a2668.js"
+  "/cotterscs/precache-manifest.003f42ad8d04dfb77e4902992abd3ff7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/cotterscs/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
