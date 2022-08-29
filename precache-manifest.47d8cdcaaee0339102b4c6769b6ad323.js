90d8b5201686685e71b3d1ec5c3a39bb4dbf0064
Jordan Cotter <jrdn.cttr@gmail.com>
2022-08-29 14:29:19 -0500

    Updates

---
+++ precache-manifest.47d8cdcaaee0339102b4c6769b6ad323.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aefef47005c00270be99",
+    "url": "/cotterscs/static/css/main.7769a707.chunk.css"
+  },
+  {
+    "revision": "aefef47005c00270be99",
+    "url": "/cotterscs/static/js/main.aefef470.chunk.js"
+  },
+  {
+    "revision": "22d88fc37c1257cf8cad",
+    "url": "/cotterscs/static/js/runtime~main.22d88fc3.js"
+  },
+  {
+    "revision": "3bfd54134af65bd90194",
+    "url": "/cotterscs/static/js/2.3bfd5413.chunk.js"
+  },
+  {
+    "revision": "b6b40ba9b2fefb064c866f3ae6db4f44",
+    "url": "/cotterscs/index.html"
+  }
+];
